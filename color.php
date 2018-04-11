03f076b29771493d847fc2ac18541f2f3570527b
Joshua Gatdula <joshlgatdula@gmail.com>
2018-04-11 15:45:32 +0800

    Add files via upload

---
+++ color.php
@@ -0,0 +1,13 @@
+<?php
+$color = "blue";
+$color = "red";
+$color = "blue"; 
+echo $color; 
+echo "<br>";
+$color = "red";
+echo $color;
+echo "<br>";
+$num = 55.27;
+printf("dollar to peso is %f",$num); //%.2f for 2 decimal places
+echo "<br>";
+?>
---
+++ convert.php
@@ -0,0 +1,6 @@
+<?php
+define("RATE",55.8);
+$US_dollars = 20;
+$PH_peso = $US_dollars * RATE;
+echo $PH_peso;
+?>
