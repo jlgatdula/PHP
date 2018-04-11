65fb1bb440005bafdd1990fecce95d139bf60f94
Joshua Gatdula <joshlgatdula@gmail.com>
2018-04-11 15:15:46 +0800

    Add files via upload

---
+++ helloworld.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<title>
+Hello World Script
+</title>
+<head>
+<body>
+<?php
+echo 123; //numbers not in quotation, works
+echo "<br>";
+echo "Hello World!";
+echo "<br>";
+echo "Hello","World!";
+echo "<br>";
+echo "Hello","","World!";
+echo "<br>";
+echo "Hello World!"; //enclose characters in quotation
+echo "<br>";
+echo 'Hello World!'; "Hello World!";
+echo "<br>";
+echo "Hello World!";
+echo "<br>";
+echo "Hello"; echo "World!";
+echo "<br>";
+echo "Hello\nWorld!";
+echo "<br>";
+echo "<h1>Hello<br>World!</h1>";
+echo "<br>";
+echo "<h1>Hello<br>\nWorld!</h1>";
+echo "<br>";
+?>
+</body>
+</html>
---
+++ taro.php
@@ -0,0 +1,6 @@
+<?php
+echo 'hello';
+print 'world<br>';
+$name='Taro';
+printf('My name is <br> %s',$name);
+?>
