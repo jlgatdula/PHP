977b3d0d8183adc88aea7ef56f2480e7614240ba
Joshua Gatdula <joshlgatdula@gmail.com>
2018-04-12 19:48:16 +0800

    Create my_solution.php

---
+++ my_solution.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>Structure and Errors sample page - FIX ME!!!</title>
+</head>
+<body>
+<table border=0>
+<tr>
+<td>
+<?php
+echo '<b>This should be formatted nicely to be easy to read in this document.</b>';
+echo '<b> If you can not quickly and easily find out what the errors</b><br>';
+echo '<b>on this page are, perharps you should go back and re-examine the fifth movie in this chapter...</b>';
+?>
+</td>
+</tr>
+</table>
+<?php
+echo 'And by the way, if you can figure out all the errors on this page, then you are well on your way to become a good PHP programmer. Congratulations!';
+?>
+</body>
+</html>
